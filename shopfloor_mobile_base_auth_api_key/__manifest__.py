150a2b124bc9e6acf6c69eeb71d43dbd819bcc0a
Simone Orsi <simahawk@gmail.com>
2021-11-02 10:46:34 +0100

    fixup! Add sf_mobile_base_auth_api_key: decouple auth

--- shopfloor_mobile_base_auth_api_key/__manifest__.py
+++ shopfloor_mobile_base_auth_api_key/__manifest__.py
@@ -12,6 +12,6 @@
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainer": ["simahawk"],
     "license": "AGPL-3",
-    "depends": ["shopfloor_mobile_base"],
+    "depends": ["shopfloor_mobile_base", "auth_api_key"],
     "data": ["templates/assets.xml"],
 }
